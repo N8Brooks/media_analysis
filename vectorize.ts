12fbe626f952fb16220eca2c91d82cb045f7eafe
N8Brooks <natebrooks004@gmail.com>
2022-03-06 16:48:24 -0800

    refactor: depreciate text tile

--- vectorize.ts
+++ vectorize.ts
@@ -6,7 +6,6 @@ import { N_FEATURES } from "./constants.ts";
 import { EnglishStemmer, murmurHash3 } from "./deps.ts";
 import { preprocess } from "./preprocess.ts";
 import { STOP_WORDS } from "./stop_words.ts";
-import { textTile } from "./text_tile.ts";
 import { tokenize } from "./tokenize.ts";
 
 /** Penn Treebank stemmer instance */
@@ -17,16 +16,16 @@ const segmenter = new Intl.Segmenter("en", { granularity: "sentence" });
 
 /** Binary vectors of a `text`'s paragraphs */
 export const vectorize = (text: string): Set<number>[] => {
-  const sentences = [...segmenter.segment(text)]
-    .map(({ segment }) => {
-      const sentence = preprocess(segment);
-      const words = tokenize(sentence)
-        .filter((word) => !(word in STOP_WORDS))
-        .map((word) => stemmer.stem(word));
-      return words;
-    });
-  return textTile(sentences)
-    .map((paragraph) =>
-      new Set(paragraph.map((word) => murmurHash3(word) % N_FEATURES))
-    );
+  return [...segmenter.segment(text)].map(({ segment }) => {
+    const vector: Set<number> = new Set();
+    for (const word of tokenize(preprocess(segment))) {
+      if (word in STOP_WORDS) {
+        continue;
+      }
+      const base = stemmer.stem(word);
+      const index = murmurHash3(base) % N_FEATURES;
+      vector.add(index);
+    }
+    return vector;
+  });
 };
